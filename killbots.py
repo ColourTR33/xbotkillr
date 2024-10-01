2b3d0659068dfff8bfdcd75b753da5456bd024ea
Hartley <92923074+ColourTR33@users.noreply.github.com>
2024-10-01 12:55:24 +0100

    Added login capability

--- killbots.py
+++ killbots.py
@@ -6,3 +6,29 @@ from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 driver_path = "/path/chromedriver"
+service = Service(driver_path)
+driver = webdriver.Chrome(Service=service)
+
+username="username"
+password="password"
+
+def login_to_x():
+    driver.get("https://twitter.com/login")
+    time.sleep(5)
+    username_field = driver.find_element(By.NAME, "text")
+    username_field.send_keys(username)
+    username_field.send_keys(Keys.RETURN)
+    time.sleep(5)
+
+    password_field = 
+    driver_find_element(By.NAME, "password")
+    password_field.send_keys(password)
+    password_field.send_keys(Keys.RETURN)
+    time.sleep(5)
+
+    def main():
+        login_to_x()
+        driver.quit()
+
+        if __name__ == "__name__":
+            main()
